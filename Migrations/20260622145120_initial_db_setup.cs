c83c59cc4e4ddd0c9fc31e235d93caadb04a0473
Dipsan Dhakal <dipsandhakal224@gmail.com>
2026-06-26 05:36:15 +0100

    Set up Entity Framework and database

---
+++ Migrations/20260622145120_initial_db_setup.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace marketmanagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial_db_setup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "categories",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_categories", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "products",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    barcode = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    quantityinstock = table.Column<int>(type: "int", nullable: false),
+                    expiryorrestockdate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    categoryid = table.Column<int>(type: "int", nullable: false),
+                    supplierid = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_products", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "saleitems",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    saleid = table.Column<int>(type: "int", nullable: false),
+                    productid = table.Column<int>(type: "int", nullable: false),
+                    quantity = table.Column<int>(type: "int", nullable: false),
+                    unitprice = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_saleitems", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "sales",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Transsactiondate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    totalamount = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_sales", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "stocks",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    productid = table.Column<int>(type: "int", nullable: false),
+                    lowstockthreshold = table.Column<int>(type: "int", nullable: false),
+                    status = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_stocks", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "suppliers",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    contactinfo = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_suppliers", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "categories");
+
+            migrationBuilder.DropTable(
+                name: "products");
+
+            migrationBuilder.DropTable(
+                name: "saleitems");
+
+            migrationBuilder.DropTable(
+                name: "sales");
+
+            migrationBuilder.DropTable(
+                name: "stocks");
+
+            migrationBuilder.DropTable(
+                name: "suppliers");
+        }
+    }
+}
---
+++ data/marketdbcontext.cs
@@ -0,0 +1,22 @@
+﻿using marketmanagement.models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace marketmanagement.data
+{
+    internal class marketdbcontext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public Microsoft.EntityFrameworkCore.DbSet<category> categories { get; set; }
+        public Microsoft.EntityFrameworkCore.DbSet<stock> stocks { get; set; }
+        public Microsoft.EntityFrameworkCore.DbSet<sale> sales { get; set; }
+        public Microsoft.EntityFrameworkCore.DbSet<saleitem> saleitems { get; set; }
+        public Microsoft.EntityFrameworkCore.DbSet<product> products { get; set; }
+        public Microsoft.EntityFrameworkCore.DbSet<supplier> suppliers { get; set; }
+        protected override void OnConfiguring(Microsoft.EntityFrameworkCore.DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=localsupermarket_db;Trusted_Connection=True;");
+        }
+    }
+}
